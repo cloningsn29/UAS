d04fc66a6c24530ad123802f85c2f6bf69efa19e
cloningsn29 <sahrulnurfaizi6@gmail.com>
2021-07-12 18:10:20 +0700

    UAS

---
+++ app/src/main/java/com/sahrul_nurfaizi/usercruduas/MainActivity.java
@@ -0,0 +1,72 @@
+package com.sahrul_nurfaizi.usercruduas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.sahrul_nurfaizi.usercruduas.adapter.UserAdapter;
+import com.sahrul_nurfaizi.usercruduas.model.GetUser;
+import com.sahrul_nurfaizi.usercruduas.model.User;
+import com.sahrul_nurfaizi.usercruduas.rest.ApiClient;
+import com.sahrul_nurfaizi.usercruduas.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert;
+    ApiInterface mApiInterface;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    //private RecyclerView.LayoutManager mLayoutManager;
+    public static MainActivity ma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert=(Button)findViewById((R.id.btnInsert));
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,InsertActivity.class));
+            }
+        });
+        mRecyclerView =(RecyclerView) findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        //mLayoutManager = new LinearLayoutManager(this);
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        ma = this;
+        refresh();
+    }
+
+    public void refresh(){
+        Call<GetUser> userCall = mApiInterface.getUser();
+        userCall.enqueue(new Callback<GetUser>() {
+            @Override
+            public void onResponse(Call<GetUser> call, Response<GetUser> response) {
+                List<User> userList=response.body().getData();
+                Log.d("Retrofit Get", "Jumlah data User "+String.valueOf(userList.size()));
+                mAdapter = new UserAdapter(MainActivity.this, userList);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<GetUser> call, Throwable t) {
+                Log.e("Retrofit Get", t.toString());
+            }
+        });
+    }
+}
